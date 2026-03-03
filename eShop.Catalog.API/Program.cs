61df9461c07e78351185011c6aec8bdf108fea27
Paolo Paci <paolopci@yahoo.it>
2026-03-03 11:39:57 +0100

    refactoring initial filtering e sorting lesson

--- eShop.Catalog.API/Program.cs
+++ eShop.Catalog.API/Program.cs
@@ -23,40 +23,46 @@ builder.Services
     // rende disponibile la definizione di filtro personalizzata che hai scritto (campi e operazioni consentite);
     .AddType<ProductFilterInputType>()
     // Configura le regole globali di paginazione per i campi che usano paging.
-    .ModifyPagingOptions(options =>
-    {
-        // Numero di elementi restituiti di default quando il client non specifica first/last.
-        options.DefaultPageSize = 2;
-        // Limite massimo di elementi richiedibili in una singola pagina.
-        options.MaxPageSize = 5;
-        // Disabilita la paginazione backward (before/last).
-        // I “boundary di paging” sono i limiti che definiscono quale fetta di risultati
-        // vuoi.Nel tuo caso (Relay-style paging GraphQL) sono tipicamente:first e after
-        // per andare avanti;last e before per andare indietro.
-        options.AllowBackwardPagination = false;
-        // Richiede che il client invii esplicitamente i boundary di paging.
-        // Con RequirePagingBoundaries = true, il client deve passare almeno
-        // un limite (first o last), quindi non può fare query paginata “senza confini”.
-        // Questo evita richieste troppo vaghe o potenzialmente pesanti.
-        options.RequirePagingBoundaries = true;
-    })
+    // .ModifyPagingOptions(options =>
+    // {
+    //     // Numero di elementi restituiti di default quando il client non specifica first/last.
+    //     options.DefaultPageSize = 2;
+    //     // Limite massimo di elementi richiedibili in una singola pagina.
+    //     options.MaxPageSize = 5;
+    //     // Disabilita la paginazione backward (before/last).
+    //     // I “boundary di paging” sono i limiti che definiscono quale fetta di risultati
+    //     // vuoi.Nel tuo caso (Relay-style paging GraphQL) sono tipicamente:first e after
+    //     // per andare avanti;last e before per andare indietro.
+    //     options.AllowBackwardPagination = false;
+    //     // Richiede che il client invii esplicitamente i boundary di paging.
+    //     // Con RequirePagingBoundaries = true, il client deve passare almeno
+    //     // un limite (first o last), quindi non può fare query paginata “senza confini”.
+    //     // Questo evita richieste troppo vaghe o potenzialmente pesanti.
+    //     options.RequirePagingBoundaries = true;
+    // })
     // Abilita la proiezione dei campi GraphQL verso IQueryable/EF.
     .AddProjections()
-    .AddFiltering(
-            c =>
-            {
-                // Abilita la funzionalità “Filtering” di HotChocolate: cioè permette ai client di scrivere query del tipo:
-                /*
-                        query {
-                          products(where: { name: { startsWith: "A" } }) {
-                            nodes { id name }
-                            }
-                        }             
-                */
-                c.AddDefaults() //Registra le regole e i tipi di filtro “di default” per i tipi comuni (int, string, datetime, ecc.). Senza, dovresti configurare a mano molte cose.
-                .BindRuntimeType<string, CustomStringOperationFilterInputType>();
-            }
-    );
+    // Abilita il middleware di filtering per gli attributi [UseFiltering]/[UseFiltering<T>].
+    .AddFiltering();
+
+//.AddProjections()
+
+
+// .AddFiltering(
+//         c =>
+//         {
+//             // Abilita la funzionalità “Filtering” di HotChocolate: cioè permette ai client di scrivere query del tipo:
+//             /*
+//                     query {
+//                       products(where: { name: { startsWith: "A" } }) {
+//                         nodes { id name }
+//                         }
+//                     }             
+//             */
+//             c.AddDefaults() //Registra le regole e i tipi di filtro “di default” per i tipi comuni (int, string, datetime, ecc.). Senza, dovresti configurare a mano molte cose.
+//             .BindRuntimeType<string, CustomStringOperationFilterInputType>();
+//         }
+// );
 
 var app = builder.Build();
 
@@ -89,12 +95,12 @@ app.Run();
 // uindi, di fatto, stai dicendo: “per le stringhe voglio esporre solo queste operazioni”.
 // Equals (eq)
 // StartsWith (startsWith)
-public class CustomStringOperationFilterInputType : StringOperationFilterInputType
-{
-    protected override void Configure(IFilterInputTypeDescriptor descriptor)
-    {
-        descriptor.Operation(DefaultFilterOperations.Equals).Type<StringType>();
-        descriptor.Operation(DefaultFilterOperations.StartsWith).Type<StringType>();
+// public class CustomStringOperationFilterInputType : StringOperationFilterInputType
+// {
+//     protected override void Configure(IFilterInputTypeDescriptor descriptor)
+//     {
+//         descriptor.Operation(DefaultFilterOperations.Equals).Type<StringType>();
+//         descriptor.Operation(DefaultFilterOperations.StartsWith).Type<StringType>();
 
-    }
-}
+//     }
+// }
