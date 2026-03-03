0511d1160c26d70b08c6a568c503b61826a92c28
Paolo Paci <paolopci@yahoo.it>
2026-03-03 10:03:54 +0100

    add new method in query.cs for Brand and Types

--- eShop.Catalog.API/Types/Query.cs
+++ eShop.Catalog.API/Types/Query.cs
@@ -7,33 +7,50 @@ namespace eShop.Catalog.API.Types;
 
 public class Query()
 {
+    [UsePaging(DefaultPageSize = 1, MaxPageSize = 10)]
+    [UseProjection]
+    public IQueryable<Brand> GetBrands(CatalogContext context)
+        => context.Brands;
+
+    [UseFirstOrDefault]
+    [UseProjection]
+    public IQueryable<Brand> GetBrandById(int id, CatalogContext context)
+        => context.Brands.Where(t => t.Id == id);
 
     // se lascio questi setting in UsePaging questi sovrascrivono quelli che ho impostato in program.cs
     // con l'uso .ModifyPagingOptions(options => ....
     [UsePaging(DefaultPageSize = 1, MaxPageSize = 11)]
     [UseProjection] // con questo attributo ho  inserito un middleware nella mia pipeline
     public IQueryable<Product> GetProducts(CatalogContext context)
-    {
-        return context.Products;
-    }
+        => context.Products;
 
     [UseFirstOrDefault]// con questo attributo ho  inserito un middleware nella mia pipeline
     [UseProjection]// con questo attributo ho  inserito un middleware nella mia pipeline
-    // importante è l'ordinecon il quale li inseriamo si parte dall'alto verso il basso
+    // importante: è l'ordine con il quale li inseriamo, si parte dall'alto verso il basso
     public IQueryable<Product> GetProductById(int id, CatalogContext context)
-                    => context.Products.Where(p => p.Id == id);
+        => context.Products.Where(p => p.Id == id);
 
     [UsePaging]
     [UseProjection]
     public IQueryable<Product> GetProductsByType(int typeId, CatalogContext context)
-                    => context.Products
-                        .AsNoTracking()
-                        .Where(p => p.TypeId == typeId);
+        => context.Products
+            .AsNoTracking()
+            .Where(p => p.TypeId == typeId);
 
     [UsePaging]
     [UseProjection]
     public IQueryable<Product> GetProductsByBrand(int brandId, CatalogContext context)
-                    => context.Products
-                        .AsNoTracking()
-                        .Where(p => p.BrandId == brandId);
+        => context.Products
+            .AsNoTracking()
+            .Where(p => p.BrandId == brandId);
+
+    [UsePaging]
+    [UseProjection]
+    public IQueryable<ProductType> GetProductTypes(CatalogContext context)
+        => context.ProductTypes;
+
+    [UseFirstOrDefault]
+    [UseProjection]
+    public IQueryable<ProductType> GetProductTypeById(int id, CatalogContext context)
+        => context.ProductTypes.Where(t => t.Id == id);
 }
